623749552eec5cc9cc0901b479bfb76353453c9e
Jacob Eddison <jacob.eddison@qa.com>
2019-09-13 10:11:37 +0100

    created delete pet function by id in PetController

--- src/main/java/com/qa/controllers/PetController.java
+++ src/main/java/com/qa/controllers/PetController.java
@@ -1,7 +1,25 @@
 package com.qa.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.qa.models.Pets;
+import com.qa.repository.PetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin()
 public class PetController {
+
+    @Autowired
+    private PetRepository repository;
+
+    //delete a single pet by id
+    @RequestMapping(value = "pets/{id}", method = RequestMethod.DELETE)
+    public Pets deletePet(@PathVariable Long id) {
+        Pets existing = repository.findOne(id);
+        repository.delete(existing);
+        return existing;
+    }
+
+
 }
+
