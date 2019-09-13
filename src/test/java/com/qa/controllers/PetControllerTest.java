d669afa288d8faf655a5069db96fefd0adfa6584
johndoubleub <up741132@myport.ac.uk>
2019-09-13 11:20:56 +0100

    Created tests for deletePet and updatePets

--- src/test/java/com/qa/controllers/PetControllerTest.java
+++ src/test/java/com/qa/controllers/PetControllerTest.java
@@ -1,5 +1,6 @@
 package com.qa.controllers;
 
+import com.qa.models.Pets;
 import com.qa.repository.PetRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,12 +12,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PetControllerTest {
 
     @InjectMocks
-    private PetController perController;
+    private PetController petController;
 
     @Mock
     private PetRepository repository;
@@ -29,11 +33,28 @@ public class PetControllerTest {
 
     @Test
     public void updatePetsTest(){
+        Pets pet1 = new Pets();
+        pet1.setName("Luka");
+        pet1.setColour("black");
+        pet1.setId(3L);
 
+        when(repository.findOne(3L)).thenReturn(pet1);
+        when(repository.saveAndFlush(pet1)).thenReturn(pet1);
+
+        assertEquals(petController.updatePets(3L, pet1).getId(),new Long(3L));
     }
 
     @Test
     public void deletePetTest(){
+        Pets pet1 = new Pets();
+        pet1.setName("Pip");
+        pet1.setColour("Brown");
+        pet1.setId(5L);
+
+
+        when(repository.findOne(5L)).thenReturn(pet1);
+
+        assertEquals(petController.deletePet(5L).getName(), "Pip");
 
     }
 
