b6748b0aef47100e2204b709cbc9c245736be957
johndoubleub <up741132@myport.ac.uk>
2019-09-13 10:16:10 +0100

    Added request mapping for updatePets, also added updateAll method in Pets repository

--- src/main/java/com/qa/controllers/PetController.java
+++ src/main/java/com/qa/controllers/PetController.java
@@ -1,7 +1,27 @@
 package com.qa.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.qa.models.Pets;
+import com.qa.repository.PetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin()
 public class PetController {
+
+    @Autowired
+    private PetRepository repository;
+
+    @Transactional
+    @RequestMapping(value = "pets/{id}", method = RequestMethod.PUT)
+    public Pets updatePets(@PathVariable Long id, @RequestBody Pets pet){
+        Pets existing = repository.findOne(id);
+
+        existing.updateAll(pet);
+
+        return repository.saveAndFlush(existing);
+    }
+
+
 }
